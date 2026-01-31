3b5c390115101b0d82769407a97b237ec33315d7
𝗭ɪᴏɴɪx / <adityashakya000001@gmail.com>
2026-01-31 16:34:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-#fucku
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({
+        "success": False,
+        "error": "kya re bhenkelode",
+        "status": 404
+    }), 404
+
+@app.route("/")
+def home():
+    return "toremayekochodo"
+  
